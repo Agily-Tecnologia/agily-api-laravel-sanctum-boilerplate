0fd5afc35cfd4cfc96a3fe3a5151ab0122c84838
Paulo Cardoso <paulo@agily.com.br>
2021-01-30 18:36:48 -0300

    validando excecoes pra /api/*

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -2,7 +2,9 @@
 
 namespace App\Exceptions;
 
+use Illuminate\Auth\AuthenticationException;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Illuminate\Validation\ValidationException;
 use Throwable;
 
 class Handler extends ExceptionHandler
@@ -34,7 +36,18 @@ class Handler extends ExceptionHandler
     public function register()
     {
         $this->renderable(function (\Exception $e, $request) {
-            return response()->json($e->getMessage(), 400);
+            if ($request->is("api/*")) {
+                if ($e instanceof ValidationException) {
+                    return response()->json($e->errors(), $e->status);
+                }
+
+                if ($e instanceof AuthenticationException) {
+                    return response()->json($e->getMessage(), 401);
+                }
+
+                $statusCode = !empty($e->getCode()) ? $e->getCode() : 500;
+                return response()->json($e->getMessage(), $statusCode);
+            }
         });
 
         $this->reportable(function (Throwable $e) {
